5edaf25b1bab6cc06b0c563d6380e613d57213f4
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-09-28 17:14:38 +0100

    Snapshot

---
+++ docs/source/elements.rst
@@ -0,0 +1,45 @@
+.. Copyright (c) 2019, J. D. Mitchell
+
+   Distributed under the terms of the GPL license version 3.
+
+   The full license is in the file LICENSE, distributed with this software.
+
+Elements
+========
+
+This page contains an overview summary of the functionality in
+``libsemigroups`` for defining elements of semigroups. 
+
+Partial transformations
+~~~~~~~~~~~~~~~~~~~~~~~
+
+.. toctree::
+   :maxdepth: 1
+
+   _generated/libsemigroups__permhelper
+   _generated/libsemigroups__ppermhelper
+   _generated/libsemigroups__transfhelper
+   api/pperm
+   api/ptrans
+   api/perm
+   api/transf
+   
+Matrices
+~~~~~~~~
+
+.. toctree::
+   :maxdepth: 1
+
+   bmat
+   api/projmaxplus
+   api/mosr
+
+Partitioned binary relations and bipartitions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. toctree::
+   :maxdepth: 1
+
+   api/pbr
+   api/bipart
+   api/blocks
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <pybind11/chrono.h>
+#include <pybind11/functional.h>
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -63,6 +65,10 @@ PYBIND11_MODULE(libsemigroups_pybind11, m) {
         .. autofunction:: silo
     )pbdoc";
 
+  ////////////////////////////////////////////////////////////////////////
+  // element.hpp
+  ////////////////////////////////////////////////////////////////////////
+
   py::class_<libsemigroups::Transformation<uint8_t>>(m, "Transf256")
       .def(py::init<std::vector<uint8_t> const &>())
       .def("rank", &libsemigroups::Transformation<uint8_t>::crank,
@@ -81,29 +87,256 @@ PYBIND11_MODULE(libsemigroups_pybind11, m) {
           py::is_operator())
       .def("__repr__", &detail::transf_repr);
 
+  ////////////////////////////////////////////////////////////////////////
+  // bmat8.hpp
+  ////////////////////////////////////////////////////////////////////////
+
   py::class_<libsemigroups::BMat8>(m, "BMat8")
-      .def(py::init<>())
+      .def(py::init<>(), R"pbdoc(
+      Returns an uninitalised BMat8.
+      )pbdoc")
       .def(py::init<uint64_t>())
       .def(py::init<BMat8 const &>())
       .def(py::init<std::vector<std::vector<bool>> const &>())
       .def("__eq__", &BMat8::operator==)
       .def("__lt__", &BMat8::operator<)
-      .def("get", &BMat8::get)
-      .def("set", &BMat8::set)
-      .def("to_int", &BMat8::to_int)
-      .def("transpose", &BMat8::transpose)
+      .def("get", &BMat8::get, py::arg("i"), py::arg("j"),
+           R"pbdoc(
+           Returns the entry in the (i, j)th position.
+           )pbdoc")
+      .def("set", &BMat8::set, py::arg("i"), py::arg("j"), py::arg("val"),
+           R"pbdoc(
+           Sets the (i, j)th entry to ``val``.
+           )pbdoc")
+      .def("to_int", &BMat8::to_int,
+           R"pbdoc(
+          Returns the integer representation of the ``BMat8``, that is
+          an integer obtained by interpreting an 8 x 8 ``BMat8`` as a
+          sequence of 64 bits (reading rows left to right, from top to bottom)
+          and then realising this sequence as an integer.
+           )pbdoc")
+      .def("transpose", &BMat8::transpose,
+           R"pbdoc(
+           Returns the transpose of ``self``.
+           )pbdoc")
       .def(py::self * py::self)
       .def_static("random", py::overload_cast<>(&BMat8::random))
       .def_static("random", py::overload_cast<size_t>(&BMat8::random))
-      .def("swap", &BMat8::swap)
-      .def("row_space_basis", &BMat8::row_space_basis)
-      .def("col_space_basis", &BMat8::col_space_basis)
-      .def("rows", &BMat8::rows)
-      .def("row_space_size", &BMat8::row_space_size)
-      .def("nr_rows", &BMat8::nr_rows)
-      .def("is_regular_element", &BMat8::is_regular_element)
-      .def("one", &BMat8::one)
-      .def("__repr__", &libsemigroups::detail::to_string<BMat8>);
+      .def("swap", &BMat8::swap, py::arg("other"),
+           R"pbdoc(
+            Swaps the contents of ``self`` and ``other``.
+           )pbdoc")
+      .def("row_space_basis", &BMat8::row_space_basis,
+           R"pbdoc(
+           This method returns a ``BMat8`` whose non-zero rows form a basis for
+           the row space of ``self``.
+           )pbdoc")
+      .def("col_space_basis", &BMat8::col_space_basis,
+           R"pbdoc(
+           This method returns a ``BMat8`` whose non-zero columns form a basis for
+           the column space of ``self``.
+           )pbdoc")
+      .def("rows", &BMat8::rows,
+           R"pbdoc(
+           This method returns a list of integers representing the rows of
+           ``self``. The list will always be of length 8, even if ``self``
+           was constructed with fewer rows.
+           )pbdoc")
+      .def("row_space_size", &BMat8::row_space_size,
+           R"pbdoc(
+           Returns the size of the row space of ``self``.
+           )pbdoc")
+      .def("nr_rows", &BMat8::nr_rows,
+           R"pbdoc(
+           Returns the number of non-zero rows in ``self``.
+           )pbdoc")
+      .def("is_regular_element", &BMat8::is_regular_element,
+           R"pbdoc(
+           Check whether ``self`` is a regular element of the full boolean
+           matrix monoid of appropriate dimension.
+           )pbdoc")
+      .def("one", &BMat8::one,  // py::arg("dim") = 8,
+           R"pbdoc(
+           This method returns the ``BMat8`` with the first ``dim`` entries in
+           the main diagonal equal to ``1`` and every other value equal to ``0``.
+           )pbdoc")
+      .def("__repr__", &libsemigroups::detail::to_string<BMat8>)
+      .def("nr_cols", &libsemigroups::bmat8_helpers::nr_cols,
+           R"pbdoc(
+           Returns the number of non-zero columns in ``self``.
+           )pbdoc")
+      .def("column_space_size", &libsemigroups::bmat8_helpers::col_space_size,
+           R"pbdoc(
+           Returns the size of the row space of ``self``.
+           )pbdoc")
+      .def("minimum_dim", &libsemigroups::bmat8_helpers::minimum_dim,
+           R"pbdoc(
+            This member function returns the maximal ``i`` such that row ``i``
+            or column ``i`` contains a ``1``.
+            )pbdoc");
+
+  ////////////////////////////////////////////////////////////////////////
+  // cong.hpp
+  ////////////////////////////////////////////////////////////////////////
+
+  py::enum_<libsemigroups::congruence_type>(m, "congruence_type")
+      .value("left", libsemigroups::congruence_type::left)
+      .value("right", libsemigroups::congruence_type::right)
+      .value("twosided", libsemigroups::congruence_type::twosided)
+      .export_values();
+
+  py::enum_<libsemigroups::tril>(m, "tril")
+      .value("True", libsemigroups::tril::TRUE)
+      .value("False", libsemigroups::tril::FALSE)
+      .value("unknown", libsemigroups::tril::unknown)
+      .export_values();
+
+  py::class_<libsemigroups::Congruence>(m, "Congruence")
+      .def(py::init<libsemigroups::congruence_type>())
+      .def(py::init<libsemigroups::congruence_type,
+                    libsemigroups::FpSemigroup &>())
+      .def("set_nr_generators", &libsemigroups::Congruence::set_nr_generators,
+           py::arg("n"),
+           R"pbdoc(
+           Set the number of generators of the congruence.
+
+           :param n: the number of generators.
+           :type n: int
+
+           :return: (None)
+           )pbdoc")
+      .def("nr_generators", &libsemigroups::Congruence::nr_generators,
+           R"pbdoc(
+           Returns the number of generators.
+
+           :return: The number of generators of the semigroup of the congruence that an object of this type represents, or libsemigroups::UNDEFINED.
+           )pbdoc")
+      .def("add_pair",
+           py::overload_cast<word_type const &, word_type const &>(
+               &libsemigroups::Congruence::add_pair),
+           py::arg("u"), py::arg("v"),
+           R"pbdoc(
+           Add a generating pair to the congruence.
+
+           :param u: a word (list of integers) over the generators of the semigroup.
+           :type u: list
+           :param v: a word (list of integers) over the generators of the semigroup.
+           :type v: list
+
+           :return: (None)
+           )pbdoc")
+      // overload cast didn't work for report_every
+      .def("report_every",
+           (void (libsemigroups::Congruence::*)(std::chrono::nanoseconds)) &
+               libsemigroups::Runner::report_every,
+           py::arg("t"),
+           R"pbdoc(
+           Set the minimum elapsed time between reports.
+
+           :param t: the amount of time (in nanoseconds) between reports.
+           :type t: int
+
+           :return: (None)
+           )pbdoc")
+      .def("report", &libsemigroups::Congruence::report,
+           R"pbdoc(
+           Check if it is time to report.
+
+           :return: A bool.
+           )pbdoc")
+      .def("report_why_we_stopped",
+           &libsemigroups::Congruence::report_why_we_stopped,
+           R"pbdoc(
+           Report why Runner::run stopped.
+
+           :return: (None)
+           )pbdoc")
+      .def("kill", &libsemigroups::Congruence::kill,
+           R"pbdoc(
+           Stop Runner::run from running (thread-safe).
+
+           :return: (None).
+           )pbdoc")
+      .def("run", &libsemigroups::Congruence::run,
+           R"pbdoc(
+           Attempt to enumerate the congruence.
+
+           :return: (None)
+           )pbdoc")
+      .def("run_for",
+           (void (libsemigroups::Congruence::*)(std::chrono::nanoseconds)) &
+               libsemigroups::Runner::run_for,
+           py::arg("t"),
+           R"pbdoc(
+           Run for a specified amount of time.
+
+           :param t: the time in nanoseconds to run for.
+           :type t: int
+
+           :return: (None)
+
+           Example
+           -------
+
+           .. code-block:: python
+
+              from datetime import timedelta
+              from libsemigroups_pybind11 import Congruence
+              from libsemigroups_pybind11 import congruence_type
+
+              C = Congruence(congruence_type.twosided)
+              C.set_nr_generators(1)
+              C.add_pair([0] * 1000, [0] * 999)
+              C.run_for(timedelta(microseconds=10))
+           )pbdoc")
+      .def("run_until",
+           (void (libsemigroups::Congruence::*)(std::function<bool()> &)) &
+               libsemigroups::Runner::run_until,
+           py::arg("func"),
+           R"pbdoc(
+           Run until a nullary predicate returns true or Runner::finished.
+
+           :param func: a function.
+           :type func: func
+
+           :return: (None)
+           )pbdoc")
+      .def("less", &libsemigroups::Congruence::less, py::arg("u"), py::arg("v"),
+           R"pbdoc(
+           This function returns ``True`` if the congruence class of ``u`` is
+           less than the class of ``v`` in a total ordering of congruence
+           classes.
+
+           :param u: a word (list of integers) over the generators of the semigroup.
+           :type u: list
+           :param v: a word (list of integers) over the generators of the semigroup.
+           :type v: list
+
+           :return: ``True`` if the class of ``u`` is less than that of ``v``.
+           )pbdoc")
+      .def("const_contains", &libsemigroups::Congruence::const_contains,
+           py::arg("u"), py::arg("v"),
+           R"pbdoc(
+           Check if a pair of words is known to belong to the congruence.
+
+           :param u: a word (list of integers) over the generators of the semigroup.
+           :type u: list
+           :param v: a word (list of integers) over the generators of the semigroup.
+           :type v: list
+
+           :return: libsemigroups::tril::TRUE if the words u and v are known to belong to the same congruence class libsemigroups::tril::FALSE if the words are known to not belong to the same congruence class libsemigroups::tril::unknown otherwise.
+           )pbdoc")
+      .def("nr_classes", &libsemigroups::Congruence::nr_classes,
+           R"pbdoc(
+           Computes the total number of classes in the congruence represented
+           by an instance of this type.
+
+           :return: The number of congruences classes of this if this number is finite, or libsemigroups::POSITIVE_INFINITY in some cases if this number is not finite.
+           )pbdoc");
+
+  ////////////////////////////////////////////////////////////////////////
+  // siso.hpp
+  ////////////////////////////////////////////////////////////////////////
 
   m.def(
       "silo",
@@ -139,6 +372,39 @@ PYBIND11_MODULE(libsemigroups_pybind11, m) {
       )pbdoc");
 
   m.def(
+      "sislo",
+      [](std::string const &alphabet, std::string const &first,
+         std::string const &last) {
+        return py::make_iterator(
+            libsemigroups::cbegin_sislo(alphabet, first, last),
+            libsemigroups::cend_sislo(alphabet, first, last));
+      },
+      py::arg("alphabet"), py::arg("first"), py::arg("last"),
+      R"pbdoc(
+      Returns an iterator to strings in short-lex order (sislo).
+
+      :param alphabet: the alphabet
+      :type alphabet: str
+      :param first: the first string
+      :type first: str
+      :param last: one past the last string
+      :type last: str
+
+      :return: An iterator to strings over the alphabet ``alphabet`` in short-lex order and in the range ``[first, last)``.
+
+      Example
+      -------
+      .. code-block:: python
+
+         [x for x in sislo("ba", "b", "aaa")]
+         # ['b', 'a', 'bb', 'ba', 'ab', 'aa', 'bbb', 'bba', 'bab', 'baa', 'abb', 'aba', 'aab']
+      )pbdoc");
+
+  ////////////////////////////////////////////////////////////////////////
+  // wilo.hpp
+  ////////////////////////////////////////////////////////////////////////
+
+  m.def(
       "wilo",
       [](size_t const n, size_t const upper_bound, word_type const &first,
          word_type const &last) {
@@ -169,34 +435,9 @@ PYBIND11_MODULE(libsemigroups_pybind11, m) {
          # [[0], [0, 0], [0, 1], [1], [1, 0], [1, 1]]
       )pbdoc");
 
-  m.def(
-      "sislo",
-      [](std::string const &alphabet, std::string const &first,
-         std::string const &last) {
-        return py::make_iterator(
-            libsemigroups::cbegin_sislo(alphabet, first, last),
-            libsemigroups::cend_sislo(alphabet, first, last));
-      },
-      py::arg("alphabet"), py::arg("first"), py::arg("last"),
-      R"pbdoc(
-      Returns an iterator to strings in short-lex order (sislo).
-
-      :param alphabet: the alphabet
-      :type alphabet: str
-      :param first: the first string
-      :type first: str
-      :param last: one past the last string
-      :type last: str
-
-      :return: An iterator to strings over the alphabet ``alphabet`` in short-lex order and in the range ``[first, last)``.
-
-      Example
-      -------
-      .. code-block:: python
-
-         [x for x in sislo("ba", "b", "aaa")]
-         # ['b', 'a', 'bb', 'ba', 'ab', 'aa', 'bbb', 'bba', 'bab', 'baa', 'abb', 'aba', 'aab']
-      )pbdoc");
+  ////////////////////////////////////////////////////////////////////////
+  // wislo.hpp
+  ////////////////////////////////////////////////////////////////////////
 
   m.def(
       "wislo",
