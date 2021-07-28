416d6b7407515609767b8eb26d0ca1336f27739b
James D. Mitchell <jdm3@st-andrews.ac.uk>
2021-07-28 17:32:41 +0100

    Also build libsemigroups

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,18 +7,18 @@ import sphinx_rtd_theme
 
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if on_rtd:
-        subprocess.call(
-                        "cd ../.. && etc/make-doc-sphinx.sh",
-                                shell=True,
-                                    )
+    subprocess.call(
+        "cd ../.. && pip3 install . && etc/make-doc-sphinx.sh",
+        shell=True,
+    )
 
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
     "sphinx_copybutton",
     "sphinxcontrib.bibtex",
 ]
@@ -28,38 +28,48 @@ bibtex_bibfiles = ["libsemigroups.bib"]
 autosummary_generate = True
 add_module_names = False
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 html_static_path = ["_static"]
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'libsemigroups_pybind11'
-copyright = u'2020, J. D. Mitchell'
-author = u'J. D. Mitchell'
-version = u'0.0.1'
-release = u'0.0.1'
+source_suffix = ".rst"
+master_doc = "index"
+project = u"libsemigroups_pybind11"
+copyright = u"2021, J. D. Mitchell"
+author = u"J. D. Mitchell"
+version = u"0.0.1"
+release = u"0.0.1"
 language = None
-exclude_patterns = ['_build']
-pygments_style = 'sphinx'
+exclude_patterns = ["_build"]
+pygments_style = "sphinx"
 highlight_language = "python"
 todo_include_todos = False
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
-htmlhelp_basename = 'libsemigroups_pybind11'
+html_static_path = ["_static"]
+htmlhelp_basename = "libsemigroups_pybind11"
 
 man_pages = [
-    (master_doc, 'libsemigroups_pybind11', u'libsemigroups_pybind11 Documentation',
-
-     [author], 1)
+    (
+        master_doc,
+        "libsemigroups_pybind11",
+        u"libsemigroups_pybind11 Documentation",
+        [author],
+        1,
+    )
 ]
 
 texinfo_documents = [
-    (master_doc, 'python_example', u'python_example Documentation',
-     author, 'python_example', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "python_example",
+        u"python_example Documentation",
+        author,
+        "python_example",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {"https://docs.python.org/": None}
 
-autoclass_content = 'both'
+autoclass_content = "both"
