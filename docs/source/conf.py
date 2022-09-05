7a9b12c7e3d35042503a1faaac2f6395b819ddf7
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-09-05 13:00:53 +0100

    docs: fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,6 @@ autosummary_generate = True
 add_module_names = False
 
 templates_path = ["_templates"]
-html_static_path = ["_static"]
 source_suffix = ".rst"
 master_doc = "index"
 project = u"libsemigroups_pybind11"
@@ -29,7 +28,7 @@ copyright = u"2021, J. D. Mitchell + Maria Tsalakou"
 author = u"J. D. Mitchell"
 version = u"0.1.8"
 release = u"0.1.8"
-language = None
+language = "python"
 exclude_patterns = ["_build"]
 pygments_style = "sphinx"
 highlight_language = "python"
@@ -37,7 +36,6 @@ todo_include_todos = False
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ["_static"]
 htmlhelp_basename = "libsemigroups_pybind11"
 
 man_pages = [
