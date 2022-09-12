02da55bb331ab5db10eabb9067e54f009ebed49f
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-09-12 08:32:09 +0100

    Bump libsemigroups version to v2.2.2

--- libsemigroups_pybind11/tools.py
+++ libsemigroups_pybind11/tools.py
@@ -25,7 +25,7 @@ def minimum_libsemigroups_version():
     Returns the minimum required version of libsemigroups required to make
     this work.
     """
-    return "2.2.1"
+    return "2.2.2"
 
 
 if PKGCONFIG_IMPORTED:
