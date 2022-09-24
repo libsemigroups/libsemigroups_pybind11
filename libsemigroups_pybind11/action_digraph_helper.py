f28746548dcfb611e4404c04b2e43c6539267e1c
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-09-24 11:51:31 +0100

    action-digraph: add action_digraph_helper::make

---
+++ libsemigroups_pybind11/action_digraph_helper.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022, J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+# pylint: disable=no-name-in-module, invalid-name, unused-import
+
+"""
+This package provides the user-facing python part of libsemigroups_pybind11 for
+the action_digraph_helper namespace from libsemigroups.
+"""
+
+from _libsemigroups_pybind11 import action_digraph_helper_make as make
