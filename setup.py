7ad7e1ec75235d649e6a13175b70fe49765cd57f
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-10-03 15:01:16 +0100

    Update version numbers etc for 0.4.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from tools import (  # pylint: disable=import-error, wrong-import-position
     minimum_libsemigroups_version,
 )
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 
 def compare_version_numbers(supplied, required):
