7eb85e54e58f9d9c8343be57309fd65881625af2
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-10-04 10:13:43 +0100

    changelog: fix formatting

--- etc/deploy-doc.sh
+++ etc/deploy-doc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if ! [[ -d "gh-pages" ]] ; then
+    echo -e "Error, the \"gh-pages\" directory is not present:"
+    echo -e "git clone --branch=gh-pages git@github.com:libsemigroups/libsemigroups_pybind11.git gh-pages"
+    exit 1 
+fi
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 make clean-doc # to remove old doc files
