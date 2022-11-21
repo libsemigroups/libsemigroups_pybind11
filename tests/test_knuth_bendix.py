81796fbed9ae695ada8407152373352f376b0001
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-11-21 15:01:56 +0000

    knuth-bendix: resolve issues with alphabet encoding

--- tests/test_knuth_bendix.py
+++ tests/test_knuth_bendix.py
@@ -172,3 +172,158 @@ def test_027():
 
 def test_running_and_state():
     check_running_and_state(KnuthBendix)
+
+
+def test_alphabet_bug():
+    k = KnuthBendix()
+    k.set_alphabet(128)
+    assert list(k.alphabet()) == [
+        "\x01",
+        "\x02",
+        "\x03",
+        "\x04",
+        "\x05",
+        "\x06",
+        "\x07",
+        "\x08",
+        "\t",
+        "\n",
+        "\x0b",
+        "\x0c",
+        "\r",
+        "\x0e",
+        "\x0f",
+        "\x10",
+        "\x11",
+        "\x12",
+        "\x13",
+        "\x14",
+        "\x15",
+        "\x16",
+        "\x17",
+        "\x18",
+        "\x19",
+        "\x1a",
+        "\x1b",
+        "\x1c",
+        "\x1d",
+        "\x1e",
+        "\x1f",
+        " ",
+        "!",
+        '"',
+        "#",
+        "$",
+        "%",
+        "&",
+        "'",
+        "(",
+        ")",
+        "*",
+        "+",
+        ",",
+        "-",
+        ".",
+        "/",
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        ":",
+        ";",
+        "<",
+        "=",
+        ">",
+        "?",
+        "@",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "[",
+        "\\",
+        "]",
+        "^",
+        "_",
+        "`",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "{",
+        "|",
+        "}",
+        "~",
+        "\x7f",
+        "\x80",
+    ]
+    k.add_rule(k.alphabet(127), "a")
+    kk = KnuthBendix()
+    kk.set_alphabet(k.alphabet())
+    assert kk.alphabet() == k.alphabet()
+    assert k.active_rules() == [("a", "\x80")]
+    k.set_identity(k.alphabet(127))
+    kk.set_identity(127)
+    assert k.identity() == "\x80"
+    assert kk.identity() == "\x80"
+    k.set_inverses(k.alphabet())
+    assert k.inverses() == k.alphabet()
+    assert k.equal_to("\x80", "a")
+    assert k.normal_form("\x80") == "\x80"
+    assert k.normal_form("a") == "\x80"
+    k.validate_letter("\x80")
+    k.validate_word("\x80")
+    assert k.char_to_uint("\x80") == 127
+    assert k.uint_to_char(127) == "\x80"
+    assert k.string_to_word("\x80") == [127]
+    assert k.word_to_string([127]) == "\x80"
