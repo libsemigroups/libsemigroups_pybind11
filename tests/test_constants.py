89b2000e39a76f33d427ebaad195607b3fbf288c
Joseph Edwards <josephdavidedwards@gmail.com>
2024-03-08 17:11:08 +0000

    Add LIMIT_MAX

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -14,44 +14,128 @@ from libsemigroups_pybind11 import (
     POSITIVE_INFINITY,
     NEGATIVE_INFINITY,
     UNDEFINED,
+    LIMIT_MAX,
 )
 
 
-def test_operators():
-    """Test the comparison operators for constants in libsemigroups"""
-    assert POSITIVE_INFINITY > NEGATIVE_INFINITY
-    assert NEGATIVE_INFINITY < POSITIVE_INFINITY
-    assert not NEGATIVE_INFINITY == POSITIVE_INFINITY
-    assert NEGATIVE_INFINITY != POSITIVE_INFINITY
-    assert not 6 == POSITIVE_INFINITY
-    assert 6 != POSITIVE_INFINITY
-    assert not POSITIVE_INFINITY == 6
-    assert POSITIVE_INFINITY != 6
-    assert not 6 == NEGATIVE_INFINITY
-    assert 6 != NEGATIVE_INFINITY
-    assert not NEGATIVE_INFINITY == 6
-    assert NEGATIVE_INFINITY != 6
-    assert not POSITIVE_INFINITY == NEGATIVE_INFINITY
-    assert POSITIVE_INFINITY != NEGATIVE_INFINITY
-    assert -6 > NEGATIVE_INFINITY
-    assert 6 > NEGATIVE_INFINITY
-    assert NEGATIVE_INFINITY < 6
-    assert NEGATIVE_INFINITY < -6
-    assert not -6 < NEGATIVE_INFINITY
-    assert not 6 < NEGATIVE_INFINITY
-    assert not NEGATIVE_INFINITY > 6
-    assert not NEGATIVE_INFINITY > -6
-    assert -6 < POSITIVE_INFINITY
-    assert 6 < POSITIVE_INFINITY
-    assert POSITIVE_INFINITY > 6
-    assert POSITIVE_INFINITY > -6
-    assert not -6 > POSITIVE_INFINITY
-    assert not 6 > POSITIVE_INFINITY
-    assert not POSITIVE_INFINITY < 6
-    assert not POSITIVE_INFINITY < -6
-    assert POSITIVE_INFINITY == 2147483646
-    assert NEGATIVE_INFINITY == -2147483648
-    assert 2147483646 == POSITIVE_INFINITY
-    assert -2147483648 == NEGATIVE_INFINITY
+def test_UNDEFINED():
+    assert UNDEFINED == UNDEFINED
+    assert not (UNDEFINED == POSITIVE_INFINITY)
+    assert not (UNDEFINED == NEGATIVE_INFINITY)
+    assert not (UNDEFINED == LIMIT_MAX)
+    assert not (UNDEFINED == 0)
     assert 18446744073709551615 == UNDEFINED
     assert UNDEFINED == 18446744073709551615
+
+    assert not (UNDEFINED != UNDEFINED)
+    assert UNDEFINED != POSITIVE_INFINITY
+    assert UNDEFINED != NEGATIVE_INFINITY
+    assert UNDEFINED != LIMIT_MAX
+    assert UNDEFINED != 0
+    assert 0 != UNDEFINED
+
+    assert not (UNDEFINED > UNDEFINED)
+    assert not (UNDEFINED < UNDEFINED)
+
+
+def test_POSITIVE_INFINITY():
+
+    assert POSITIVE_INFINITY == POSITIVE_INFINITY
+    assert POSITIVE_INFINITY == 2147483646
+    assert 2147483646 == POSITIVE_INFINITY
+    assert not (POSITIVE_INFINITY == UNDEFINED)
+    assert not (POSITIVE_INFINITY == NEGATIVE_INFINITY)
+    assert not (POSITIVE_INFINITY == LIMIT_MAX)
+
+    assert not (POSITIVE_INFINITY == 0)
+    assert not (0 == POSITIVE_INFINITY)
+
+    # operator!=
+    assert not (POSITIVE_INFINITY != POSITIVE_INFINITY)
+    assert POSITIVE_INFINITY != UNDEFINED
+    assert POSITIVE_INFINITY != NEGATIVE_INFINITY
+    assert POSITIVE_INFINITY != LIMIT_MAX
+
+    assert not (POSITIVE_INFINITY == 0)
+    assert 0 != POSITIVE_INFINITY
+
+    # operator<
+    assert not (POSITIVE_INFINITY < 0)
+    assert not (POSITIVE_INFINITY < POSITIVE_INFINITY)
+    assert 0 < POSITIVE_INFINITY
+    assert 1000 < POSITIVE_INFINITY
+    assert NEGATIVE_INFINITY < POSITIVE_INFINITY
+
+    # operator>
+    assert not (POSITIVE_INFINITY > POSITIVE_INFINITY)
+    assert POSITIVE_INFINITY > 0
+    assert not (0 > POSITIVE_INFINITY)
+    assert POSITIVE_INFINITY > 100
+    assert POSITIVE_INFINITY > NEGATIVE_INFINITY
+
+
+def test_NEGATIVE_INFINITY():
+    assert NEGATIVE_INFINITY == NEGATIVE_INFINITY
+    assert NEGATIVE_INFINITY == -2147483648
+    assert -2147483648 == NEGATIVE_INFINITY
+    assert not (NEGATIVE_INFINITY == UNDEFINED)
+    assert not (NEGATIVE_INFINITY == POSITIVE_INFINITY)
+    assert not (NEGATIVE_INFINITY == LIMIT_MAX)
+
+    assert not (NEGATIVE_INFINITY == 0)
+    assert not (0 == NEGATIVE_INFINITY)
+
+    # operator!=
+    assert not (NEGATIVE_INFINITY != NEGATIVE_INFINITY)
+    assert NEGATIVE_INFINITY != UNDEFINED
+    assert NEGATIVE_INFINITY != POSITIVE_INFINITY
+    assert NEGATIVE_INFINITY != LIMIT_MAX
+
+    assert NEGATIVE_INFINITY != 0
+    assert 0 != NEGATIVE_INFINITY
+
+    # operator<
+    assert NEGATIVE_INFINITY < 0
+    assert not (NEGATIVE_INFINITY < NEGATIVE_INFINITY)
+    assert not (0 < NEGATIVE_INFINITY)
+    assert not (1000 < NEGATIVE_INFINITY)
+    assert not (POSITIVE_INFINITY < NEGATIVE_INFINITY)
+
+    # operator>
+    assert not (NEGATIVE_INFINITY > NEGATIVE_INFINITY)
+    assert not (NEGATIVE_INFINITY > 0)
+    assert 0 > NEGATIVE_INFINITY
+    assert not (NEGATIVE_INFINITY > 100)
+    assert not (NEGATIVE_INFINITY > POSITIVE_INFINITY)
+
+
+def test_LIMIT_MAX():
+    assert LIMIT_MAX == LIMIT_MAX
+    assert not (LIMIT_MAX == UNDEFINED)
+    assert not (LIMIT_MAX == POSITIVE_INFINITY)
+    assert not (LIMIT_MAX == NEGATIVE_INFINITY)
+
+    assert not (LIMIT_MAX == -1)
+    assert not (-1 == LIMIT_MAX)
+
+    # operator!=
+    assert not (LIMIT_MAX != LIMIT_MAX)
+    assert LIMIT_MAX != UNDEFINED
+    assert LIMIT_MAX != POSITIVE_INFINITY
+    assert LIMIT_MAX != NEGATIVE_INFINITY
+
+    assert LIMIT_MAX != -1
+    assert -1 != LIMIT_MAX
+
+    # operator>
+    assert not (LIMIT_MAX > LIMIT_MAX)
+    assert not (LIMIT_MAX < LIMIT_MAX)
+
+    assert LIMIT_MAX - 1
+    assert 1 - LIMIT_MAX
+    assert LIMIT_MAX > 0
+    assert not (0 > LIMIT_MAX)
+    assert 0 < LIMIT_MAX
+    assert 100 < LIMIT_MAX
+    assert not (0 > LIMIT_MAX)
+    assert not (100 > LIMIT_MAX)
