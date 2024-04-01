57e585e652f22185a96d9dac423ed05d9c13babe
Joseph Edwards <josephdavidedwards@gmail.com>
2024-04-01 19:37:18 +0100

    Add docstring and signature processor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,8 @@
 import os
 import subprocess
 import sphinx_rtd_theme
+import re
+
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -25,7 +27,7 @@ templates_path = ["_templates"]
 source_suffix = ".rst"
 master_doc = "index"
 project = "libsemigroups_pybind11"
-copyright = "2021-2023, J. D. Mitchell + Maria Tsalakou"
+copyright = "2021-2024, J. D. Mitchell + Maria Tsalakou"
 author = "J. D. Mitchell + Maria Tsalakou"
 version = "0.10.1"
 release = "0.10.1"
@@ -60,3 +62,35 @@ man_pages = [
 intersphinx_mapping = {"python": ("https://docs.python.org/", None)}
 
 autoclass_content = "both"
+
+type_replacements = {
+    r"_libsemigroups_pybind11.KnuthBendixRewriteTrie": r"KnuthBendix",
+    r"libsemigroups::Presentation<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >": r"Presentation",
+}
+# to_replace = {"method", "function", "class"}
+
+
+def change_doc(app, what, name, obj, options, lines):
+    # if what in to_replace:
+    for i, line in enumerate(lines):
+        changes = 0
+        for typename, repl in type_replacements.items():
+            line, n = re.subn(typename, repl, line)
+            changes += n
+        if changes > 0:
+            lines[i] = line
+
+
+def change_sig(app, what, name, obj, options, signature, return_annotation):
+    # if what in to_replace:
+    for typename, repl in type_replacements.items():
+        if signature is not None:
+            signature = re.sub(typename, repl, signature)
+        if return_annotation is not None:
+            return_annotation = re.sub(typename, repl, return_annotation)
+    return signature, return_annotation
+
+
+def setup(app):
+    app.connect("autodoc-process-docstring", change_doc)
+    app.connect("autodoc-process-signature", change_sig)
