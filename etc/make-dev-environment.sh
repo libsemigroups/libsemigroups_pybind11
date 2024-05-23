a8f57a7021cfe590f17e6d35ce477c835147275d
Joe Edwards <80713360+Joseph-Edwards@users.noreply.github.com>
2024-05-23 22:21:35 +0100

    Create an environment for developing (#165)

---
+++ etc/make-dev-environment.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+is_sourced() {
+    if [ -n "$ZSH_VERSION" ]; then
+        case $ZSH_EVAL_CONTEXT in *:file:*) return 0 ;; esac
+    else
+        case ${0##*/} in dash | -dash | bash | -bash | ksh | -ksh | sh | -sh) return 0 ;; esac
+    fi
+    return 1 # NOT sourced.
+}
+
+is_sourced && sourced=1 || sourced=0
+if [[ $sourced -ne 1 ]]; then
+    echo This script must be sourced, rather than run directly. Please try again with:
+    echo source $(basename "$0")
+    exit
+fi
+
+unset sourced
+
+if [[ $# -eq 0 ]]; then
+    dev_env_pkg_manager=mamba
+else
+    dev_env_pkg_manager=$1
+fi
+
+if { $dev_env_pkg_manager env list | grep 'libsemigroups_pybind11_dev'; } >/dev/null 2>&1; then
+    echo The environment libsemigroups_pybind11_dev already exists. Stopping ...
+    unset dev_env_pkg_manager
+    return
+else
+    echo Making libsemigroups_pybind11_dev environment using $dev_env_pkg_manager ...
+    echo
+    $dev_env_pkg_manager env create -f dev-environment.yml
+    if [[ $? -ne 0 ]]; then
+        echo The environment was not successfully created. Stopping ...
+        unset dev_env_pkg_manager
+        return
+    fi
+    echo Activating libsemigroups_pybind11_dev ...
+    echo
+    $dev_env_pkg_manager activate libsemigroups_pybind11_dev
+    if [[ $? -ne 0 ]]; then
+        echo The environment cannot be activated. Please try activating it yourself, and then
+        echo run the set-conda-environment-variables.sh script to set the environment variables
+        unset dev_env_pkg_manager
+        return
+    fi
+fi
+
+echo Setting environment variables ...
+echo
+./etc/set-conda-environment-variables.sh -m $dev_env_pkg_manager
+
+echo
+echo Reactivating environment ...
+echo
+
+$dev_env_pkg_manager deactivate
+if [[ $? -ne 0 ]]; then
+    unset dev_env_pkg_manager
+    return
+fi
+$dev_env_pkg_manager activate libsemigroups_pybind11_dev
+if [[ $? -ne 0 ]]; then
+    unset dev_env_pkg_manager
+    return
+fi
+
+echo
+echo Checking environment variables ...
+echo
+echo LD_LIBRARY_PATH: $LD_LIBRARY_PATH
+echo PKG_CONFIG_PATH: $PKG_CONFIG_PATH
+
+unset dev_env_pkg_manager
