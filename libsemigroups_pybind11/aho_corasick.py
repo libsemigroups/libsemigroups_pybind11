350ea5eb37b0ef402b0f46fe21927490be242db3
Joseph Edwards <josephdavidedwards@gmail.com>
2024-05-24 16:16:18 +0100

    Make aho accessible

---
+++ libsemigroups_pybind11/aho_corasick.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024, Joseph Edwards
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# pylint:disable=no-name-in-module, unused-import
+from _libsemigroups_pybind11 import add_word, rm_word, traverse_from, traverse
