343b3294c88dec0caa995560498a8ad7c908f00e
Joseph Edwards <josephdavidedwards@gmail.com>
2024-10-02 22:28:41 +0100

    Add tests

--- tests/test_ukkonen.py
+++ tests/test_ukkonen.py
@@ -18,6 +18,7 @@ import pytest
 
 from libsemigroups_pybind11 import (
     POSITIVE_INFINITY,
+    LibsemigroupsError,
     UNDEFINED,
     Ukkonen,
     ukkonen,
@@ -122,6 +123,8 @@ def test_000_c():
 
     assert len(ukkonen.maximal_piece_prefix(t, [0, 0, 4, 0, 0, 0])) == 2
     assert len(ukkonen.maximal_piece_prefix(t, [0, 1, 2, 3])) == 1
+    assert ukkonen.length_maximal_piece_prefix(t, [0, 0, 4, 0, 0, 0]) == 2
+    assert ukkonen.length_maximal_piece_prefix(t, [0, 1, 2, 3]) == 1
 
 
 def test_002():
@@ -156,6 +159,9 @@ def test_003_a():
     ukkonen.add_word(t, [1, 2, 4])
     assert t.number_of_distinct_words() == 2
 
+    assert t.index([0, 1, 2]) == 0
+    assert t.index([1, 2, 4]) == 1
+    assert t.index([0, 1, 3]) == UNDEFINED
     assert ukkonen.number_of_distinct_subwords(t) == 10
 
     assert ukkonen.is_subword(t, [])
@@ -202,8 +208,9 @@ def test_003_b():
     assert ukkonen.maximal_piece_suffix(t, [0, 1, 2]) == [1, 2]
     assert ukkonen.maximal_piece_suffix(t, [1, 2, 4]) == []
 
-    assert ukkonen.maximal_piece_suffix(t, [0, 1, 2]) == [1, 2]
-    assert ukkonen.maximal_piece_suffix(t, [1, 2, 4]) == []
+    assert ukkonen.length_maximal_piece_prefix(t, [0, 1, 2]) == 0
+    assert ukkonen.length_maximal_piece_suffix(t, [0, 1, 2]) == 2
+    assert ukkonen.length_maximal_piece_suffix(t, [1, 2, 4]) == 0
 
     assert ukkonen.number_of_pieces(t, [0, 1, 2]) == POSITIVE_INFINITY
     assert ukkonen.number_of_pieces(t, [0, 1, 2]) == POSITIVE_INFINITY
@@ -216,10 +223,7 @@ def test_003_b():
 
 def test_004():
     t = Ukkonen()
-    ukkonen.add_word(t, [0, 1, 2])
-    ukkonen.add_word(t, [0])
-    ukkonen.add_word(t, [1])
-    ukkonen.add_word(t, [2])
+    ukkonen.add_words(t, [[0, 1, 2], [0], [1], [2]])
 
     assert ukkonen.number_of_pieces(t, [0, 1, 2]) == 3
     assert ukkonen.pieces(t, [0, 1, 2]) == [[0], [1], [2]]
@@ -230,11 +234,9 @@ def test_004():
     assert ukkonen.number_of_pieces(t, [2]) == 1
     assert ukkonen.pieces(t, [2]) == [[2]]
 
-    ukkonen.add_word(t, [0, 1, 2, 8, 4, 5, 6, 7])
-    ukkonen.add_word(t, [0, 1, 2])
-    ukkonen.add_word(t, [8, 4, 5])
-    ukkonen.add_word(t, [5, 6])
-    ukkonen.add_word(t, [5, 6, 7])
+    ukkonen.add_words(
+        t, [[0, 1, 2, 8, 4, 5, 6, 7], [0, 1, 2], [8, 4, 5], [5, 6], [5, 6, 7]]
+    )
 
     assert t.number_of_distinct_words() == 8
     assert t.number_of_words() == 9
@@ -273,3 +275,111 @@ def test_004():
         [8, 4, 5],
         [6, 7],
     ]
+
+
+def test_005():
+    t = Ukkonen()
+    ukkonen.add_word(t, [0, 0, 4, 0, 0, 0])
+
+    s = ukkonen.traverse(t, [])
+    assert s[0].v == 0
+    assert s[0].pos == 0
+    assert s[1] == []
+
+    s = ukkonen.traverse(t, [4])
+    assert s[0].v == 4
+    assert s[0].pos == 1
+    assert s[1] == [4]
+
+    s = ukkonen.traverse(t, [4, 0])
+    assert s[0].v == 4
+    assert s[0].pos == 2
+    assert s[1] == [4, 0]
+
+    s = ukkonen.traverse(t, [4, 0, 0, 0])
+    assert s[0].v == 4
+    assert s[0].pos == 4
+
+    s = ukkonen.traverse(t, [0])
+    assert s[0].v == 2
+    assert s[0].pos == 1
+
+    s = ukkonen.traverse(t, [0, 4])
+    assert s[0].v == 3
+    assert s[0].pos == 1
+
+    s = ukkonen.traverse(t, [0, 4, 0, 0, 0])
+    assert s[0].v == 3
+    assert s[0].pos == 4
+    assert s[0] == Ukkonen.State(3, 4)
+
+    s = ukkonen.traverse(t, [0, 0, 2])
+    assert t.distance_from_root(t.nodes()[s[0].v])
+
+
+def test_006():
+    t = Ukkonen()
+    ukkonen.add_word(t, [0, 0, 4, 0, 0, 0])
+
+    st = Ukkonen.State()
+    ukkonen.traverse(t, st, [])
+    assert st.v == 0
+    assert st.pos == 0
+
+    ukkonen.traverse(t, st, [4])
+    assert st.v == 4
+    assert st.pos == 1
+
+
+def test_007():
+    t = Ukkonen()
+    ukkonen.add_words(t, [[0, 1, 2], [0], [1], [2]])
+
+    nodes = t.nodes()
+    root = nodes[0]
+    assert root.is_root()
+    assert not root.is_leaf()
+    assert len(root.children) == 7
+    assert root.l == 0
+    assert root.r == 0
+    assert root.parent == UNDEFINED
+    assert root.child(0) == 5
+    assert nodes[nodes[5].parent] == root
+    assert root.length() == 0
+
+
+def test_008():
+    t = Ukkonen()
+    ukkonen.add_words(
+        t,
+        [
+            [0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2],
+            [0],
+            [1],
+            [2],
+            [4, 2, 1, 2, 3, 4],
+            [4, 2, 1, 2],
+            [2, 3, 4],
+            [1, 2, 3, 4],
+            [4, 2, 3, 4],
+        ],
+    )
+    assert t.length_of_distinct_words() == 27
+    assert t.length_of_words() == 33
+    assert t.max_word_length() == 6
+    assert t.multiplicity(0) == 3
+    assert t.multiplicity(1) == 1
+    assert t.number_of_distinct_words() == 9
+    assert t.number_of_words() == 11
+    assert len(t.nodes()) == 48
+    t.init()
+    assert t.length_of_distinct_words() == 0
+    assert t.length_of_words() == 0
+    assert t.max_word_length() == 0
+    with pytest.raises(LibsemigroupsError):
+        t.multiplicity(0)
+    assert t.number_of_distinct_words() == 0
+    assert t.number_of_words() == 0
+    assert len(t.nodes()) == 1
