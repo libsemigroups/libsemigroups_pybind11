7b7ebca4d21a0ecd69d9316638af12869b5529c7
Joseph Edwards <josephdavidedwards@gmail.com>
2024-11-06 13:17:43 +0000

    Move docs from py to cpp

--- libsemigroups_pybind11/paths.py
+++ libsemigroups_pybind11/paths.py
@@ -13,80 +13,16 @@ This package provides the user-facing python part of libsemigroups_pybind11 for
 the libsemigroups::paths namespace from libsemigroups.
 """
 
-from typing import Union
-
 from _libsemigroups_pybind11 import (
     Paths,
     ReversiblePaths,
-    POSITIVE_INFINITY,
-    PositiveInfinity,
 )
 
-
-def count(p: Union[Paths, ReversiblePaths]) -> Union[int, PositiveInfinity]:
-    """
-    Get the size of the range. This function returns the number of paths
-    remaining in the range (in particular, if :any:`next` is called then the
-    return value of :any:`count` decreases by ``1``).
-
-    :returns:
-       The number of paths remaining in the range.
-    :rtype:
-       int | PositiveInfinity
-
-    :raises LibsemigroupsError: if ``source() == UNDEFINED``.
-    """
-    result = p._count()
-    if result == POSITIVE_INFINITY:
-        return POSITIVE_INFINITY
-    return result
+from .detail.decorators import may_return_positive_infinity
 
 
-def max_method(*args):
-    """
-    max(*args, **kwargs)
+Paths.count = may_return_positive_infinity(Paths.count)
+Paths.max = may_return_positive_infinity(Paths.max)
 
-        Overloaded function.
-
-        .. py:method:: max(self: Paths) -> int | POSITIVE_INFINITY
-            :noindex:
-
-            Get the maximum length of path in the range. This function returns the
-            current maximum length of paths in the range. The initial value is
-            :any:`POSITIVE_INFINITY`.
-
-            :returns:
-               The maximum length of paths in the range.
-
-            :rtype:
-               int | PositiveInfinity
-
-        .. py:method:: max(self: Paths, val: int | POSITIVE_INFINITY) -> Paths
-            :noindex:
-
-            Set the maximum length of path in the range.
-
-            This function can be used to set the maximum length of path that will be
-            contained in the range. If this function is not called, then the range will
-            contain paths of unbounded length (possibly infinitely many).
-
-            :param val: the maximum path length.
-            :type val: int
-
-            :returns: ``self``.
-            :rtype: Paths
-    """
-    if len(args) == 1:
-        result = args[0]._max()
-        if result == POSITIVE_INFINITY:
-            return POSITIVE_INFINITY
-        return result
-    if len(args) > 1:
-        return args[0]._max(*args[1:])
-    raise TypeError
-
-
-Paths.count = count
-Paths.max = max_method
-ReversiblePaths.count = count
-ReversiblePaths.max = max_method
+ReversiblePaths.count = may_return_positive_infinity(ReversiblePaths.count)
+ReversiblePaths.max = may_return_positive_infinity(ReversiblePaths.max)
