d9d3b3226e74962946fad66eb25c11ee555070eb
Joseph Edwards <josephdavidedwards@gmail.com>
2025-09-05 18:12:38 +0100

    Delta: test delta

---
+++ tests/test_delta.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2022-2024 J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+"""
+This module contains some tests for the delta function.
+"""
+
+from datetime import datetime, timedelta
+from time import sleep
+
+from libsemigroups_pybind11 import delta
+
+
+def test_delta():
+    current_time = datetime.now()
+    wait_time = 10**-3
+    sleep(wait_time)
+    diff = delta(current_time)
+    assert isinstance(diff, timedelta)
+    assert diff > timedelta(seconds=wait_time)
