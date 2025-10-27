d1e2fced1ea713dfaa608c0be71d4cbc591a67a0
Joseph Edwards <josephdavidedwards@gmail.com>
2025-10-27 12:48:55 +0000

    Automatically create requirements archive

--- etc/make-requirements.sh
+++ etc/make-requirements.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 set -e
-CUSTOM_COMPILE_COMMAND="./etc/$(basename $0)" pip-compile --upgrade --all-build-deps --all-extras --strip-extras pyproject.toml 
+
+echo "Constructing requiremens list and writing to requirements.txt ..."
+
+# The following requires pip-compile, which is made available through the package pip-tools
+CUSTOM_COMPILE_COMMAND="./etc/$(basename $0)" pip-compile --upgrade --all-build-deps --all-extras --strip-extras pyproject.toml
+echo "Done"
+echo
+
+echo "Copying requirements into requirements-archive/ ..."
+
+VERSION=$(python -c "import sys; print(sys.version_info[1])")
+cp requirements.txt requirements-archive/py$VERSION-requirements.txt 
+echo "Done"
