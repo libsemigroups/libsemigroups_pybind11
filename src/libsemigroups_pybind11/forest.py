ae8fd410f13476672ae71e60c24c782ea3182cb6
James Mitchell <jdm3@st-andrews.ac.uk>
2025-11-06 16:26:52 +0000

    forest: add PathsToRoots

--- src/libsemigroups_pybind11/forest.py
+++ src/libsemigroups_pybind11/forest.py
@@ -7,18 +7,27 @@
 # The full license is in the file LICENSE, distributed with this software.
 
 """
-This page contains the documentation for various helper functions for
-manipulating :any:`Forest` objects. All such functions are contained in
+This page contains the documentation for various helper classes and functions for
+manipulating :any:`Forest` objects. All such classes and functions are contained in
 the submodule ``forest``.
 """
 
 from typing_extensions import Self as _Self
 
 from _libsemigroups_pybind11 import (  # pylint: disable=no-name-in-module
+    forest_PathsFromRoots as PathsFromRoots,
+    forest_PathsToRoots as PathsToRoots,
     forest_depth as depth,
     forest_dot as dot,
     forest_is_root as is_root,
-    forest_path_to_root as path_to_root,
-    forest_path_from_root as path_from_root,
     forest_max_label as max_label,
+    forest_path_from_root as path_from_root,
+    forest_path_to_root as path_to_root,
 )
+
+# The following fools sphinx into thinking that PathsToRoots is not an
+# aliases.
+PathsFromRoots.__module__ = __name__
+PathsFromRoots.__name__ = "PathsFromRoots"
+PathsToRoots.__module__ = __name__
+PathsToRoots.__name__ = "PathsToRoots"
