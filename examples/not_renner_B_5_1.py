88f8fef97746e7c43b1de2de1a408633b3f81b02
Joseph Edwards <josephdavidedwards@gmail.com>
2025-11-11 01:06:27 +0000

    pylint: fix redefined-outer-name

--- examples/not_renner_B_5_1.py
+++ examples/not_renner_B_5_1.py
@@ -22,17 +22,17 @@ def witness(wg: WordGraph, p: Presentation) -> tuple[list[int], list[int]]:
             return (p.rules[i], p.rules[i + 1])
 
 
-p = examples.not_renner_type_B_monoid(5, 1)
-q = examples.renner_type_B_monoid(5, 1)
+p1 = examples.not_renner_type_B_monoid(5, 1)
+q1 = examples.renner_type_B_monoid(5, 1)
 
 # We iterate 2-sided congruences of the monoid defined by "p" with the aim of
 # finding one which is not compatible with the relations in "q"
-twosided_congs = Sims2(p)
-wg = twosided_congs.find_if(4096, lambda wg: check_incompat(wg, q))
-word_graph.number_of_nodes_reachable_from(wg, 0)  # 2177
-wg.induced_subgraph(0, 2177)
-witness(wg, q)  # ([5, 0, 1, 2, 3, 4, 0, 1, 2, 3, 0, 1, 2, 0, 1, 0, 5], [10])
+twosided_congs = Sims2(p1)
+wg1 = twosided_congs.find_if(4096, lambda wg: check_incompat(wg, q1))
+word_graph.number_of_nodes_reachable_from(wg1, 0)  # 2177
+wg1.induced_subgraph(0, 2177)
+witness(wg1, q1)  # ([5, 0, 1, 2, 3, 4, 0, 1, 2, 3, 0, 1, 2, 0, 1, 0, 5], [10])
 # meaning that the paths with source 0 labelled by these two words should lead to the same
 # node but do not.
-word_graph.follow_path(wg, 0, [5, 0, 1, 2, 3, 4, 0, 1, 2, 3, 0, 1, 2, 0, 1, 0, 5])  # 6
-word_graph.follow_path(wg, 0, [10])  # 7
+word_graph.follow_path(wg1, 0, [5, 0, 1, 2, 3, 4, 0, 1, 2, 3, 0, 1, 2, 0, 1, 0, 5])  # 6
+word_graph.follow_path(wg1, 0, [10])  # 7
