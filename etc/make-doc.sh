8fb201ef5ae08f33efd2b8264617f28118cea4bc
James Swent <jake@osprey21.com>
2025-11-20 14:17:31 +0000

    doc: add to-table generation to make-doc.sh

--- etc/make-doc.sh
+++ etc/make-doc.sh
@@ -7,6 +7,23 @@ else
   PYTHON="python3"
 fi
 
+echo "Checking whether to build the to-table . . ."
+if command -v pdflatex && command -v inkscape 2>&1 >/dev/null; then
+  cd docs/pictures
+  echo "Building to-table . . ."
+  pdflatex to-table.tex
+  inkscape --pdf-poppler --export-type="svg" -o to-table.svg to-table.pdf
+  if [ $? -eq 0 ]; then
+    echo Successfully created to-table.svg
+  else
+    echo Warning: there was an issue with inkscape when creating to-table.svg
+    echo Please check the state of to-table.svg
+  fi
+  cd ../..
+else
+  echo "Not building to-table"
+fi
+
 $PYTHON etc/check-docstring-indentation.py
 
 cd docs/
