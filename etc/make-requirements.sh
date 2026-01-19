7eefdddf9f7eb5ee2c5bee7e5844a595d347f9a7
Joseph Edwards <josephdavidedwards@gmail.com>
2026-01-19 12:04:20 +0000

    Remove requirements-archive

--- etc/make-requirements.sh
+++ etc/make-requirements.sh
@@ -7,9 +7,3 @@ echo "Constructing requiremens list and writing to requirements.txt ..."
 CUSTOM_COMPILE_COMMAND="./etc/$(basename $0)" pip-compile --upgrade --all-build-deps --all-extras --strip-extras pyproject.toml
 echo "Done"
 echo
-
-echo "Copying requirements into requirements-archive/ ..."
-
-VERSION=$(python -c "import sys; print(sys.version_info[1])")
-cp requirements.txt requirements-archive/py$VERSION-requirements.txt 
-echo "Done"
