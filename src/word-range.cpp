aca49e1e498f0477dfe44fbbf5d224c3131693af
James Mitchell <jdm3@st-andrews.ac.uk>
2026-08-18 17:07:48 +0100

    word-range: shortlex -> lenlex in to_human_readable_repr

--- src/word-range.cpp
+++ src/word-range.cpp
@@ -82,13 +82,13 @@ Example
   ...      .alphabet_size(2) \
   ...      .min(1) \
   ...      .max(5)
-  <WordRange of length 30 with letters in [0, 2) in shortlex order>
+  <WordRange of length 30 with letters in [0, 2) in lenlex order>
   >>> words.order()
   <Order.lenlex: 1>
 
   >>> from libsemigroups_pybind11 import ToString
   >>> words.alphabet_size(1).min(0).max(10)
-  <WordRange of length 10 with letters in [0, 1) in shortlex order>
+  <WordRange of length 10 with letters in [0, 1) in lenlex order>
 
   >>> strings = words | ToString("a")
   >>> list(strings)
@@ -483,13 +483,13 @@ Example
   ...        .alphabet("ba") \
   ...        .min(1) \
   ...        .max(5)
-  <StringRange of length 30 in shortlex order>
+  <StringRange of length 30 in lenlex order>
   >>> strings.order()
   <Order.lenlex: 1>
 
   >>> from libsemigroups_pybind11 import ToWord
   >>> strings.alphabet("a").min(0).max(10)
-  <StringRange of length 10 in shortlex order>
+  <StringRange of length 10 in lenlex order>
 
   >>> wrds = strings | ToWord("a")
   >>> list(wrds)
